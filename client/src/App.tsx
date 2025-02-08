9a0f1454a09e882c8285d160c05f45293a83dd90
KhaledAlkilani <khaledkelany@gmail.com>
2025-02-08 13:30:28 +0200

    updated package json scripts for open api to generate frontend interfaces automatically and wrap them in api.ts, installed MUI, added proxy to package.json, tested server connection and fetch data from db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { api, Player } from "./api/api";
+import { Button } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    api
+      .getPlayers()
+      .then(setPlayers)
+      .catch((err) => setError(err.message));
+  }, []);
+
+  if (error) return <p>Error: {error}</p>;
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "start",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <h1>Players</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          {players.map((player) => (
+            <li key={player.id}>
+              {player.firstName} {player.lastName}
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
