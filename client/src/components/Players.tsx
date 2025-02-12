cbf7b88f554c60a09790770a76574eab7d3bcafa
KhaledAlkilani <khaledkelany@gmail.com>
2025-02-12 10:31:35 +0200

    re-organized file structure in frontend

---
+++ client/src/components/Players.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { api, Player } from "../api/api";
+import { Typography } from "@mui/material";
+
+// This was created for testing. Can remove it or update it when start app implementation.
+
+const Players = () => {
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    api
+      .getPlayers()
+      .then(setPlayers)
+      .catch(() =>
+        setError(
+          "Server is offline or an error occurred. Please try again later."
+        )
+      );
+  }, []);
+
+  if (error)
+    return <Typography sx={{ p: 2, color: "red" }}>{error}</Typography>;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "start",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <h1>Players</h1>
+      <div>
+        <ul>
+          {players.map((player) => (
+            <li key={player.id}>
+              {player.firstName} {player.lastName}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Players;
